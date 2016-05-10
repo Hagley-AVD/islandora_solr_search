db21e30bcedecf761d0729e70e415ea4e965b023
mdemers <mdemers@hagley.org>
2016-05-10 12:57:22 +0000

    swapped facet spans

--- theme/islandora-solr-facet.tpl.php
+++ theme/islandora-solr-facet.tpl.php
@@ -11,12 +11,12 @@
 <ul class="<?php print $classes; ?>">
   <?php foreach($buckets as $key => $bucket): ?>
     <li>
-      <?php print $bucket['link']; ?>
-      <span class="count">(<?php print $bucket['count']; ?>)</span>
       <span class="plusminus">
         <?php print $bucket['link_plus']; ?>
         <?php print $bucket['link_minus']; ?>
       </span>
+      <?php print $bucket['link']; ?>
+      <span class="count">(<?php print $bucket['count']; ?>)</span>
     </li>
   <?php endforeach; ?>
 </ul>
